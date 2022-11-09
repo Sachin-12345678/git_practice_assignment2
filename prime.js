c1c8e006bdbe40b5a3b48a4f3ae0c6793ac4bec9
chetan <chetangulave14@gmail.com>
2022-11-09 21:53:56 +0530

    file corrected

--- prime.js
+++ prime.js
@@ -2,8 +2,12 @@
 let num=13;
 let count=0;
 for(let i=1; i<num; i++){
-if(num%1==0)
-{
- 
+if(num%i==0){
+ count++;
 }
 }
+if(count==2){
+console.log(num,"is a prime number");
+}else{
+console.log(num,"is not a prime number");
+};
